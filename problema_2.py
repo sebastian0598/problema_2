b1bbfbec9244f27407efbaef2638ccfdfefa2611
sebastian <sgaviria@uniguajira.edu.co>
2020-09-16 22:08:31 -0500

    registro y  editar

--- problema_2.py
+++ problema_2.py
@@ -21,6 +21,23 @@ class agenda():
             if (opcion == "n"):
                 break
         return self.contacto
+        
+    def editar_registro(self):
+        
+        while True:
+            self.codigo = (input("digite el codigo del contacto: "))
+            self.nombre = input("digite el nombre: ")
+            self.telefono = input("digite el telefono ")
+            self.Email = input("digite el EMAIL: ")
+            self.contacto2[self.codigo] = (self.nombre,self.telefono,self.Email)
+            opcion = input("desea continuar s/n: ")
+
+            if (opcion == "n"):
+                break
+        self.contacto.update(self.contacto2)
+        for self.codigo in self.contacto:
+            print(f"codigo {self.codigo} - {self.contacto[self.codigo]}")
+        return self.contacto2
 
 def main():
     contacto1 = agenda()
